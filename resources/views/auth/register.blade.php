1a7db6751f840bbb3ffe27a1fa07cfa18da3766d
leonardsvilinskis <leonardsvilinskis@icloud.com>
2025-05-05 11:34:01 +0300

    register salabots/strada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,34 +6,7 @@
     <title>Register</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <style>
-        body {
-            background-color: #f4f7fc;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .register-container {
-            background: white;
-            padding: 40px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-            width: 100%;
-            max-width: 400px;
-            text-align: center;
-        }
-        .form-group {
-            text-align: left;
-        }
-        .btn-primary {
-            width: 100%;
-            padding: 10px;
-        }
-        .text-center {
-            margin-top: 10px;
-        }
-    </style>
+
 </head>
 <body>
     <div class="register-container">
@@ -95,9 +68,7 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script>
-        window.jQuery || document.write('<script src="{{ asset('js/vendor/jquery-1.11.2.min.js') }}"><\/script>');
-    </script>
+    <script src="{{ asset('js/vendor/jquery-1.11.2.min.js') }}"></script>
     <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/plugins.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
